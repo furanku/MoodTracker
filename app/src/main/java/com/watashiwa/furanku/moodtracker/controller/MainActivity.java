bf8d86e748070993c56f46fdded10442b83b5e84
furanku <frankensteffen@gmail.com>
2018-01-26 21:47:28 +0100

    created MVC packages, moved MainActivity to controller

--- app/src/main/java/com/watashiwa/furanku/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/watashiwa/furanku/moodtracker/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.watashiwa.furanku.moodtracker;
+package com.watashiwa.furanku.moodtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.watashiwa.furanku.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
