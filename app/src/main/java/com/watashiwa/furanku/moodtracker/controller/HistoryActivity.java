af07133eea76cd8c314e70e2fdc18e7e1f5b8239
furanku <frankensteffen@gmail.com>
2018-02-05 21:11:08 +0100

    created history activity with ita layout

---
+++ app/src/main/java/com/watashiwa/furanku/moodtracker/controller/HistoryActivity.java
@@ -0,0 +1,15 @@
+package com.watashiwa.furanku.moodtracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.watashiwa.furanku.moodtracker.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_history);
+	}
+}
