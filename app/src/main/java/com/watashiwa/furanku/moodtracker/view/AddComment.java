70f8b8204ba1d03c6a9f527cf78006fafe67fe0e
furanku <frankensteffen@gmail.com>
2018-02-05 20:59:26 +0100

    added buttons,updated

--- app/src/main/java/com/watashiwa/furanku/moodtracker/view/AddComment.java
+++ app/src/main/java/com/watashiwa/furanku/moodtracker/view/AddComment.java
@@ -10,6 +10,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,53 +21,66 @@ import com.watashiwa.furanku.moodtracker.R;
  */
 
 public class AddComment extends Dialog {
-
+	/**
+	 * This constructeur call the method addCommentDialog
+	 * @param context
+	 */
 	public AddComment(@NonNull Context context) {
 		super(context);
+		addCommentDialog();
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-			//Creates a new AlertDialog
-			AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-			//References to the specific layout
-		View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.comment_layout, null);
+	}
+
+	/**
+	 * this method creates and sets a new Alert dialog
+	 */
+	public void addCommentDialog() {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+		//References to the specific layout
+		final View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.comment_layout, null);
 		/**
 		 * Sets the view with the specific layout
 		 */
 		builder.setView(dialogView)
-				/**
-				 * sets the icon in the title Bar
-				 */
-				 .setIcon(R.drawable.ic_comment_black_48px)
 
-				/**
-				 * sets the positive button and the actions when it is clicked
-				 */
-				 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							//the user enter and valid its comment
-							//the we save it and back to the previous page
-						 }
-				 })
-						/**
-						 * set the negative button and the actions when it is clicked
-						 */
-				 .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							//the user cancel its comment we go back to the previous page
-							dialog.cancel();
-						}
+				  /**
+					* the ALertDialog will be cancellable only with a click on the Cancel button
+					*/
+			    .setCancelable(false)
+				  /**
+					* sets the icon in the title Bar
+					*/
+			    .setIcon(R.drawable.ic_comment_black_48px)
 
-				 })
+				  /**
+					* sets the title of the Alert dialog
+					*/
+			    .setTitle("Enter your comment please")
 
-			    /**
-				 * sets the title of the Alert dialog
-				 */
-			    .setTitle("Enter your comment please");
+				  /**
+					* This neutral button closes the AlertDialog
+					*/
+				  .setNeutralButton("CLOSE", new OnClickListener() {
+					  @Override
+					  public void onClick(DialogInterface dialog, int which) {
+					  	//Closes the dialog
+						  dialog.cancel();
+					  }
+				  });
+		/**
+		 * References the OK button to the id in layout
+		 */
+		final Button BUTTON_OK = dialogView.findViewById(R.id.ok_btn);
+
+		/**
+		 * references the Cancel button to the id in layout
+		 */
+		final Button BUTTON_CANCEL = dialogView.findViewById(R.id.cancel_btn);
+
 		/**
 		 * References the edit text of the alert dialog to its unique id in layout
 		 */
@@ -84,24 +98,46 @@ public class AddComment extends Dialog {
 				/**
 				 * Number max of character the user can enter
 				 */
-				Character MaxCharacter = 100;
+				final Character MAX_CHARACTER = 100;
+				/**
+				 * Disables the Ok button if there is no character entered by the user
+				 */
+				if (editText.length() > 0) BUTTON_OK.setEnabled(true);
+				else BUTTON_OK.setEnabled(false);
+
 				/**
 				 * if the user reached the max number of character a little message is shown as toast
 				 */
-				if (editText.length()== MaxCharacter){
-					Toast toast =Toast.makeText(getContext(), "Number Max of character reached ", Toast.LENGTH_LONG);
+				if (editText.length() == MAX_CHARACTER) {
+					Toast toast = Toast.makeText(getContext(), "Number Max of character reached ", Toast.LENGTH_LONG);
 					toast.show();
 				}
 			}
 
 			@Override
 			public void afterTextChanged(Editable s) {
+			}
+		});
 
+		BUTTON_OK.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//save,notify history
+			}
+		});
+
+		BUTTON_CANCEL.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Cancel all the characters entered by the user when clicking
+				editText.setText(" ");
 			}
 		});
 		//creates the builder just configured
-	   builder.create()
-				  //shows the builder just created
-				 .show();
+		//builder.create()
+		//shows the builder just created
+		builder.show();
+		//sets the OK button disabled by default
+		BUTTON_OK.setEnabled(false);
 	}
 }
